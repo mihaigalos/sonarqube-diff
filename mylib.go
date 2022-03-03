a2c7a0c938974e011e9c2312a9e53e90e78822ed
Mihai Galos <mihaigalos@gmail.com>
2022-03-03 20:34:23 +0100

    Resolve magic numbers

--- mylib.go
+++ mylib.go
@@ -17,11 +17,15 @@ func get_map_metadata(url string) map[string]string {
 	fmt.Println(doc.Find("title").Text())
 	rows := doc.Find("div", "class", "detail").Find("table").Find("tbody").FindAll("tr")
 
+	pos_rule := 0
+	pos_line := 3
+	pos_key := 6
+
 	for _, row := range rows {
 		file := row.Find("td", "class", "component").Text()
-		line := row.FindAll("td")[3].Text()
-		rule := row.FindAll("td")[0].Find("a").Text()
-		key := row.FindAll("td")[6].Text()
+		rule := row.FindAll("td")[pos_rule].Find("a").Text()
+		line := row.FindAll("td")[pos_line].Text()
+		key := row.FindAll("td")[pos_key].Text()
 
 		e := file + "," + line + "," + rule
 

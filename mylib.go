e7248eb728cd62927f85a9e6487bedc6fa9306e9
Mihai Galos <mihaigalos@gmail.com>
2022-03-03 20:29:59 +0100

    Construct internal metadata map

--- mylib.go
+++ mylib.go
@@ -7,9 +7,10 @@ import (
 	"github.com/anaskhan96/soup"
 )
 
-func Run(url string) {
+func Run(url_A string) {
 
-	resp, err := soup.Get(url)
+	data_A := make(map[string]string)
+	resp, err := soup.Get(url_A)
 	if err != nil {
 		os.Exit(1)
 	}
@@ -21,9 +22,12 @@ func Run(url string) {
 		file := row.Find("td", "class", "component").Text()
 		line := row.FindAll("td")[3].Text()
 		rule := row.FindAll("td")[0].Find("a").Text()
+		key := row.FindAll("td")[6].Text()
 
 		e := file + "," + line + "," + rule
 
-		fmt.Println(e)
+		data_A[key] = e
+
 	}
+	fmt.Println(data_A)
 }
